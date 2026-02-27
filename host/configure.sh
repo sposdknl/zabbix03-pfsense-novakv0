5a81831536f0b6fa4873bd3bc71e4454229db4f2
Novák Vojtěch <novak.vojtech@student.sposdk.cz>
2026-02-27 09:35:00 +0100

    O

--- host/configure.sh
+++ host/configure.sh
@@ -3,7 +3,7 @@ UNIQUE_HOSTNAME="ubuntu-$(uuidgen)"
 SHORT_HOSTNAME=$(echo $UNIQUE_HOSTNAME | cut -d'-' -f1,2)
 sudo cp -v /etc/zabbix/zabbix_agent2.conf /etc/zabbix/zabbix_agent2.conf-orig
 sudo sed -i "s/Hostname=Zabbix server/Hostname=$SHORT_HOSTNAME/g" /etc/zabbix/zabbix_agent2.conf
-sudo sed -i 's/Server=127.0.0.1/Server=192.168.1.100/g' /etc/zabbix/zabbix_agent2.conf
+sudo sed -i 's/Server=127.0.0.1/Server=192.168.1.10/g' /etc/zabbix/zabbix_agent2.conf
 sudo sed -i 's/ServerActive=127.0.0.1/ServerActive=192.168.1.10/g' /etc/zabbix/zabbix_agent2.conf
 sudo sed -i 's/# Timeout=3/Timeout=30/g' /etc/zabbix/zabbix_agent2.conf
 sudo sed -i 's/# HostMetadata=/HostMetadata=SPOS/g' /etc/zabbix/zabbix_agent2.conf
